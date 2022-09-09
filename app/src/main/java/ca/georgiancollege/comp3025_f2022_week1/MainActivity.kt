dadf904d159f9c21d9123a9060eb53f223bf103d
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-09-09 10:52:43 -0400

    Added Button to  Layout

--- app/src/main/java/ca/georgiancollege/comp3025_f2022_week1/MainActivity.kt
+++ app/src/main/java/ca/georgiancollege/comp3025_f2022_week1/MainActivity.kt
@@ -2,10 +2,19 @@ package ca.georgiancollege.comp3025_f2022_week1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity()
+{
+    // LifeCycle Function
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        val HelloWorldLabel = findViewById(R.id.Hello_World_Label) as TextView
+
+        
     }
 }
